575b836d6d6c84dda62573ce85a41e369702a034
darreceygor <66496611+darreceygor@users.noreply.github.com>
2021-06-28 20:42:33 -0300

    Update partial.js

--- js/partial.js
+++ js/partial.js
@@ -1,7 +1,5 @@
 window.onload = partial();
 
-
-
 function partial(event)
 {
 
@@ -46,7 +44,7 @@ function listarProd(datosProd){
             <article id="contenido${i}" class="card">
                 <div class ="item">
                     <div id="item-descripcion">
-                        <img src="${datosProd[i].url}" alt="imagen ${i}" id="img-item">
+                        <img src="${datosProd[i].url}" alt="imagen ${i}" class="img-item">
                     <p>
                         <h4>${datosProd[i].descripcion}</h4> <br>
                             
@@ -126,154 +124,6 @@ function ponerCarro(data){
                   
                
     })}
-
-
-  
-function ponerCarro2(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data2 =>{   
-
-        let carro2 = document.querySelector("#carrito2")
-        cantidad[1] = cantidad[1]+1;
-        carro2.innerHTML="";
-        carro2.innerHTML+= 
-                `${data2[1].modelo} - 
-                Cantidad: ${cantidad[1]} 
-                Precio: ${data2[1].precio*cantidad[1]}`;
-                
-                let precioTotal = document.querySelector("#totalCarro")
-
-                marca[1]=data2[1].marca; console.log(marca[1]);
-                modelo[1]=data2[1].modelo; console.log(modelo[1]);
-                precio[1]=data2[1].precio; console.log(precio[1]);
-
-                sumarCarro(data2[1].precio);
-    })
-     
-}   
-
-
-function ponerCarro3(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data3 =>{   
-
-        let carro3 = document.querySelector("#carrito3")
-        cantidad[2]+=1;
-        carro3.innerHTML="";
-        carro3.innerHTML+= 
-                `${data3[2].modelo} - 
-                Cantidad: ${cantidad[2]} 
-                Precio: ${data3[2].precio}`;
-                
-                let precioTotal = document.querySelector("#totalCarro")
-
-                sumarCarro(data3[2].precio);
-    })
-     
-}  
-
-function ponerCarro4(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data4 =>{   
-
-        let carro4 = document.querySelector("#carrito4")
-        cantidad[3]+=1;
-        carro4.innerHTML="";
-        carro4.innerHTML+= 
-                `${data4[3].modelo} - 
-                Cantidad: ${cantidad[3]} 
-                Precio: ${data4[3].precio}`;
-
-                sumarCarro(data4[3].precio);
-                
-                
-    })
-     
-}  
-
-function ponerCarro5(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data5 =>{   
-
-        let carro5 = document.querySelector("#carrito5")
-        cantidad [4]+=1;
-        carro5.innerHTML="";
-        carro5.innerHTML+= 
-                `${data5[4].modelo} - 
-                Cantidad: ${cantidad[4]} 
-                Precio: ${data5[4].precio}`;
-                
-                sumarCarro(data5[4].precio);
-                
-                
-    })
-     
-}  
-
-function ponerCarro6(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data6 =>{   
-
-        let carro6 = document.querySelector("#carrito6")
-        cantidad [5]+=1;
-        carro6.innerHTML="";
-        carro6.innerHTML+= 
-                `${data6[5].modelo} - 
-                Cantidad: ${cantidad[5]} 
-                Precio: ${data6[5].precio}`;
-                
-                sumarCarro(data6[5].precio);
-                
-                
-    })
-     
-} 
-
-function ponerCarro7(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data7 =>{   
-
-        let carro7 = document.querySelector("#carrito7")
-        cantidad [6]+=1;
-        carro7.innerHTML="";
-        carro7.innerHTML+= 
-                `${data7[6].modelo} - 
-                Cantidad: ${cantidad[6]} 
-                Precio: ${data7[6].precio}`;
-                
-                sumarCarro(data7[6].precio);
-                
-                
-    })
-     
-}
-
-function ponerCarro8(){
-    fetch('json/producto.json') 
-    .then(res => res.json()) 
-    .then(data8 =>{   
-
-        let carro8 = document.querySelector("#carrito8")
-        cantidad [7]+=1;
-        carro8.innerHTML="";
-        carro8.innerHTML+= 
-                `${data8[7].modelo} - 
-                Cantidad: ${cantidad[7]} 
-                Precio: ${data8[7].precio}`;
-                
-                sumarCarro(data8[7].precio);
-
-               
-
-    })
-     
-}
 
 function sumarCarro(valor){
     
