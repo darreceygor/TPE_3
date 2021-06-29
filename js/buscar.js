d036102ab2efc2d84ffda78e70986471bb5a1d6b
darreceygor <66496611+darreceygor@users.noreply.github.com>
2021-06-29 16:39:44 -0300

    Add files via upload

---
+++ js/buscar.js
@@ -0,0 +1,22 @@
+function Buscar() {
+    var tabla = document.getElementById('tblPersonas');
+    var busqueda = document.getElementById('txtBusqueda').value.toLowerCase();
+    var cellsOfRow="";
+    var found=false;
+    var compareWith="";
+    for (var i = 1; i < tabla.rows.length; i++) {
+        cellsOfRow = tabla.rows[i].getElementsByTagName('td');
+        found = false;
+        for (var j = 0; j < cellsOfRow.length && !found; j++) { compareWith = cellsOfRow[j].innerHTML.toLowerCase(); if (busqueda.length == 0 || (compareWith.indexOf(busqueda) > -1))
+            {
+                found = true;
+            }
+        }
+        if(found)
+        {
+            tabla.rows[i].style.display = '';
+        } else {
+            tabla.rows[i].style.display = 'none';
+        }
+    }
+}
